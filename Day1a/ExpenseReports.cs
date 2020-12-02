a768a605f70f3930570822d183495a9f4c676139
Barry Wallis <barry.wallis@outlook.com>
2020-12-01 17:59:51 -0800

    Day 1 complete.

--- Day1a/ExpenseReports.cs
+++ Day1a/ExpenseReports.cs
@@ -11,16 +11,17 @@ namespace Day1a
         private static readonly List<int> _expenses = new();
 
         /// <summary>
-        /// Process expense reports by finding the first two expenses that sum to <paramref name="sum"/> and return their
+        /// Process expense reports by finding the first three expenses that sum to <paramref name="sum"/> and return their
         /// product. If no expenses meet the criteria or their is an input format error, return null.
         /// </summary>
         /// <param name="sum">The amount that two expense reports need to sum to.</param>
-        /// <returns>The product of the two expense reports or null if none meet the criteriaor there is a format error in the
-        /// input.</returns>
+        /// <returns>The product of the three expense reports or null if none meet the criteriaor there is a format error in
+        /// the input.
+        /// </returns>
         public static int? Process(int sum)
         {
             int? result = null;
-            while (int.TryParse(Console.ReadLine(), out int expense) && !FoundExpenses(expense, sum, out result))
+            while (int.TryParse(Console.ReadLine(), out int expense) && !IfFoundExpenses(expense, sum, out result))
             {
                 // This space intentionally left blank.
             }
@@ -29,16 +30,17 @@ namespace Day1a
         }
 
         /// <summary>
-        /// Check the current expense against all previous expenses to find the two expenses that sum to <paramref name="sum"/>.
+        /// Check the current expense against all previous expenses to find the two expenses that sum to 
+        /// <paramref name="sum"/>.
         /// </summary>
         /// <param name="expense">The current expense.</param>
-        /// <param name="sum">The value that two expenses need to sum to.</param>
+        /// <param name="sum">The value that <paramref name="numberToCheck"/> expenses need to sum to.</param>
         /// <param name="result">If the two expenses sum to 2020, the product of the two expenses; otherwise null.</param>
         /// <returns>True if the two expenses sum to 2020; otherwise false.</returns>
-        private static bool FoundExpenses(int expense, int sum, out int? result)
+        private static bool IfFoundExpenses(int expense, int sum, out int? result)
         {
             result = null;
-            if (_expenses.Count == 0)
+            if (_expenses.Count < 2)
             {
                 _expenses.Add(expense);
             }
@@ -56,14 +58,18 @@ namespace Day1a
         /// </summary>
         /// <param name="expense">The current expense.</param>
         /// <param name="sum">The value that two expenses need to sum to.</param>
+        /// <param name="numberToCheck">The number of expenses to check their sum.</param>
         /// <returns>If they sum to <paramref name="sum"/>, their product; otherwise null.</returns>
         private static int? CheckExpenses(int expense, int sum)
         {
-            foreach (int previousExpense in _expenses)
+            for (int i = 0; i < _expenses.Count - 1; i++)
             {
-                if (expense + previousExpense == sum)
+                for (int j = i + 1; j < _expenses.Count; j++)
                 {
-                    return expense * previousExpense;
+                    if (expense + _expenses[i] + _expenses[j] == sum)
+                    {
+                        return expense * _expenses[i] * _expenses[j];
+                    }
                 }
             }
 
