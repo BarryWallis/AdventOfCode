22de01ef46beaedf8120f9ad70f25f390329cad0
Barry Wallis <barry.wallis@outlook.com>
2020-12-02 13:22:47 -0800

    Day 2 complete.

---
+++ Day2/PasswordValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    /// <summary>
+    /// Ensure a given password has MinimumLength <= number of Letter <= MaximumLength
+    /// </summary>
+    record PasswordValidator(int MustHavePosition1, int MustHavePosition2, char Letter)
+    {
+        /// <summary>
+        /// Test if password is valid.
+        /// </summary>
+        /// <param name="password">The password to test.</param>
+        /// <returns>True if <paramref name="password"/> has Letter at MustHavePosition and not have Letter at 
+        /// MustNotHavePosition.</returns>
+        public bool IsPasswordValid(string password) => password.Length >= MustHavePosition1
+                                                        && password.Length >= MustHavePosition2
+                                                        && (password[MustHavePosition1 - 1] == Letter
+                                                            ^ password[MustHavePosition2 - 1] == Letter);
+    }
+}
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -19,13 +19,9 @@ namespace Day2
      * 1-3 b: cdefg
      * 2-9 c: ccccccccc
      * 
-     * Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of
-     * times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a 
-     * at least 1 time and at most 3 times.
-     * 
-     * In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs
-     * at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their 
-     * respective policies.
+     * Each line gives the password policy and then the password. The password policy indicates the two positions that must be
+     * checked for the given letter. The letter must appear in one of those positions but not the other (all other letters are
+     * irrelevant.
      * 
      * How many passwords are valid according to their policies?
      */
